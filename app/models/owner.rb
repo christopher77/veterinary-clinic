30a20fd703942fba8a3ddeb7a017275b7aee4587
Angie Cristina <angiecristinagonzalessara@gmail.com>
2019-04-05 10:36:08 -0500

    Change message and quotation mark

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,6 @@
 class Owner < ApplicationRecord
   validates_each :birthday do |record, attr, value|
-    record.errors.add attr, ‘does not meet Terms of Service requirements
-    (over 18)’ if value > (18.years.ago).to_date
+    record.errors.add attr, "a owner must be at least 18 years of age "
+    if value > (18.years.ago).to_date
     end
 end
